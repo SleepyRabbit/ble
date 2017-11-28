68ae4cc896b78aac36d325d67b93a3df244c1675
侯恩星 <houenxing@houenxingdeMacBook.local>
2017-11-29 01:02:37 +0800

    update

--- ble/ListViewController.m
+++ ble/ListViewController.m
@@ -8,7 +8,7 @@
 
 #import "ListViewController.h"
 
-@interface ListViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface ListViewController ()
 @property (strong, nonatomic) IBOutlet UIStackView *stackView;
 
 @end
@@ -22,29 +22,66 @@
     _stackView.axis = UILayoutConstraintAxisVertical;
     _stackView.alignment = UIStackViewAlignmentFill;
     _stackView.distribution = UIStackViewDistributionFillEqually;   //子控件自动布局为大小相等的控件
-    _stackView.spacing = 20;
+    _stackView.spacing = 0;
     _stackView.backgroundColor = [UIColor blueColor];
 
+    CGRect stackRect = _stackView.frame;
+    NSLog(@"%f", stackRect.size.height);
+    NSLog(@"%f", stackRect.size.width);
+
+
+    CGRect tabRect = CGRectMake(10, 10, stackRect.size.width - 20, stackRect.size.height/5 - 20);
+
+    UIView *view1 = [[UIView alloc] init];
+    view1.backgroundColor = [UIColor redColor];
+
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor redColor];
+
+    UIView *view3 = [[UIView alloc] init];
+    view3.backgroundColor = [UIColor redColor];
+
+    UIView *view4 = [[UIView alloc] init];
+    view4.backgroundColor = [UIColor redColor];
+
+    UIView *view5 = [[UIView alloc] init];
+    view5.backgroundColor = [UIColor redColor];
+
     UIImage *img1 = [UIImage imageNamed:@"expression.jpg"];
-    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:img1];
+    UIImageView *imageView1 = [[UIImageView alloc] initWithFrame:tabRect];
+    imageView1.image = img1;
+//    [imageView1 layoutIfNeeded];
+    imageView1.layer.cornerRadius = 10;
+    imageView1.clipsToBounds = YES;
 
     UIImage *img2 = [UIImage imageNamed:@"expression.jpg"];
-    UIImageView *imageView2 = [[UIImageView alloc] initWithImage:img2];
+    UIImageView *imageView2 = [[UIImageView alloc] initWithFrame:tabRect];
+    imageView2.image = img2;
 
     UIImage *img3 = [UIImage imageNamed:@"expression.jpg"];
-    UIImageView *imageView3 = [[UIImageView alloc] initWithImage:img3];
+    UIImageView *imageView3 = [[UIImageView alloc] initWithFrame:tabRect];
+    imageView3.image = img3;
 
     UIImage *img4 = [UIImage imageNamed:@"expression.jpg"];
-    UIImageView *imageView4 = [[UIImageView alloc] initWithImage:img4];
+    UIImageView *imageView4 = [[UIImageView alloc] initWithFrame:tabRect];
+    imageView4.image = img4;
 
     UIImage *img5 = [UIImage imageNamed:@"expression.jpg"];
-    UIImageView *imageView5 = [[UIImageView alloc] initWithImage:img5];
+    UIImageView *imageView5 = [[UIImageView alloc] initWithFrame:tabRect];
+    imageView5.image = img5;
 
-    [_stackView addArrangedSubview:imageView1];
-    [_stackView addArrangedSubview:imageView2];
-    [_stackView addArrangedSubview:imageView3];
-    [_stackView addArrangedSubview:imageView4];
-    [_stackView addArrangedSubview:imageView5];
+
+    [view1 addSubview:imageView1];
+    [view2 addSubview:imageView2];
+    [view3 addSubview:imageView3];
+    [view4 addSubview:imageView4];
+    [view5 addSubview:imageView5];
+
+    [_stackView addArrangedSubview:view1];
+    [_stackView addArrangedSubview:view2];
+    [_stackView addArrangedSubview:view3];
+    [_stackView addArrangedSubview:view4];
+    [_stackView addArrangedSubview:view5];
 }
 
 - (void)didReceiveMemoryWarning {
