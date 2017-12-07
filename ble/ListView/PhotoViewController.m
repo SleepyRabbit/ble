ace7b58805ce71e2aafcdb3d2caf0b98e633f659
侯恩星 <houenxing@houenxingdeMacBook.local>
2017-12-08 01:39:47 +0800

    update

--- ble/ListView/PhotoViewController.m
+++ ble/ListView/PhotoViewController.m
@@ -8,7 +8,11 @@
 
 #import "PhotoViewController.h"
 
-@interface PhotoViewController ()
+@interface PhotoViewController ()<UIImagePickerControllerDelegate>
+{
+    UIImageView *_imageView;
+    UIImagePickerController *imagePickerController;
+}
 
 @end
 
@@ -17,11 +21,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
+//    _imageView = [[UIImageView alloc]initWithFrame:CGRectMake(16, 86, self.view.frame.size.width-32,self.view.frame.size.width-32)];
+//    _imageView.backgroundColor = [UIColor grayColor];
+//    [self.view addSubview:_imageView];
+
+    //调用系统相册
+    imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    imagePickerController.allowsEditing = YES;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:imagePickerController animated:NO completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark <UIImagePickerControllerDelegate>
+//按下确认按钮
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *, id> *)info {
+    [imagePickerController dismissViewControllerAnimated:NO completion:nil];
+
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+
+    CGFloat width = CGImageGetWidth(image.CGImage);
+    CGFloat height = CGImageGetHeight(image.CGImage);
+
+    NSLog(@"%f", width);
+    NSLog(@"%f", height);
+
+    _imageView = [[UIImageView alloc] initWithFrame:CGRectMake(16, 86, width, height)];
+    [_imageView setImage:image];
+    [self.view addSubview:_imageView];
+}
+
+//按下取消按钮
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    NSLog(@"cancel");
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 /*
